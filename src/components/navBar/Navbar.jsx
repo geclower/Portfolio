754d76e25ecff9ac0a4c6c81866228e2b735f907
Grace Clower <clower.grace@gmail.com>
2024-08-03 16:11:02 -0400

    got nav bar sorted with color highlighting based on page location

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,27 +1,57 @@
 import React from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function navbar() {
-  
-
-
   return (
     <div className="navbar">
-      
       <nav className="nav">
-        <Link to="/" className="navLink" id="homeNav">
-          <img id="homeIcon" src="https://i.imgur.com/m16jYp0.png"/>
-        </Link>
-        <Link to="/projects" className="navLink" id="projectNav">
-        <img id="projectIcon" src="https://i.imgur.com/vqTtA0b.png"/>
-        </Link>
-        <Link to="/about" className="navLink" id="aboutNav">
-        <img id="aboutIcon" src="https://i.imgur.com/h7gmnga.png"/>
-        </Link>
+        <NavLink
+          to="/"
+          className="navLink"
+          id="homeNav"
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? "#92DDEA" : "#f1f3f4",
+            };
+          }}
+        >
+          <img id="homeIcon" src="https://i.imgur.com/m16jYp0.png" />
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="navLink"
+          id="projectNav"
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? "#aaedd4" : "#f1f3f4",
+            };
+          }}
+        >
+          <img id="projectIcon" src="https://i.imgur.com/vqTtA0b.png" />
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="navLink"
+          id="aboutNav"
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? "#be9ddf" : "#f1f3f4",
+            };
+          }}
+        >
+          <img id="aboutIcon" src="https://i.imgur.com/h7gmnga.png" />
+        </NavLink>
 
-        <div  className="navLink" id="mailNav" onClick={(e) => {window.location.href ='mailto:clower.grace@gmail.com';}}><img id="mailIcon" src="https://i.imgur.com/CQVjnKf.png"/></div>
-        
+        <div
+          className="navLink"
+          id="mailNav"
+          onClick={(e) => {
+            window.location.href = "mailto:clower.grace@gmail.com";
+          }}
+        >
+          <img id="mailIcon" src="https://i.imgur.com/CQVjnKf.png" />
+        </div>
       </nav>
     </div>
   );
