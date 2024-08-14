afd65cc7f6b6b1ca4ef7ed17384eb5b989981bcd
Grace Clower <clower.grace@gmail.com>
2024-08-14 09:12:28 -0400

    removed project preview from home page after feedback that it came across as a mistake

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,24 +6,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="homePage">
-       <div className="project-previews-top">
-        <Link className="project-preview-wat" to="/projects/whereartthou">
-          <img
-            className="project-preview-image"
-            id="ppi-wat"
-            alt="project preview"
-            src="https://i.imgur.com/Ck5edln.png"
-          />
-        </Link>
-        <Link className="project-preview-bb" to="/projects/bwaybound">
-          <img
-            className="project-preview-image"
-            id="ppi-bb"
-            alt="project preview"
-            src="https://i.imgur.com/dzoZa8Y.png"
-          />
-        </Link>
-        </div>
+    
       <div className="introBox">
         <h3 className="introName">Hi, I'm Grace! </h3>
         <p className="introDescriptor">
@@ -31,32 +14,7 @@ function Home() {
         </p>
       </div>
       <div class="gradient"></div>
-      <div className="project-previews-bottom">
-        <Link className="project-preview-ts" to="/projects/touchstone">
-          <img
-            className="project-preview-image"
-            id="ppi-ts"
-            alt="project preview"
-            src="https://i.imgur.com/iV3kRAY.jpeg"
-          />
-        </Link>
-        <Link className="project-preview-rekt" to="/projects/rekt">
-          <img
-            className="project-preview-image"
-            id="ppi-rkt"
-            alt="project preview"
-            src="https://i.imgur.com/pnwr40r.png"
-          />
-        </Link>
-        <Link className="project-preview-wm" to="/projects/wellmet">
-          <img
-            className="project-preview-image"
-            id="ppi-wm"
-            alt="project preview"
-            src="https://i.imgur.com/bSm34Em.png"
-          />
-        </Link>
-      </div>
+     
       <Navbar />
     </div>
   );
