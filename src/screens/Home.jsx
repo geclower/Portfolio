0e7c99684bfd4c2efd3cd2032f0dc12c8d9b88f6
Grace Clower <clower.grace@gmail.com>
2024-08-14 09:14:45 -0400

    error handling 14.8.24

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./StyleSheets/Home.css";
 import Navbar from "../components/navBar/Navbar";
-import { Link } from "react-router-dom";
 
 function Home() {
   return (
